0b7a4557d9d887e0fb730fa0b7cdb5cbe184e01a
prpereiras89 <prpereiras89@gmail.com>
2020-08-26 09:20:05 -0300

    Updates

---
+++ precache-manifest.b6d2d922c1d66bf57545c69329a22bd9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2108fccc459c09cf7b4fa0e22c72ae28",
+    "url": "/website-squad-law-1/index.html"
+  },
+  {
+    "revision": "b75fde0f0f30ea1d7be3",
+    "url": "/website-squad-law-1/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "2ca44a48569454b7640e",
+    "url": "/website-squad-law-1/static/css/main.fbe4a6c7.chunk.css"
+  },
+  {
+    "revision": "b75fde0f0f30ea1d7be3",
+    "url": "/website-squad-law-1/static/js/2.aac4ea9d.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/website-squad-law-1/static/js/2.aac4ea9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ca44a48569454b7640e",
+    "url": "/website-squad-law-1/static/js/main.bbf75596.chunk.js"
+  },
+  {
+    "revision": "6587acd4820afaa8d436",
+    "url": "/website-squad-law-1/static/js/runtime-main.ebf6c1a6.js"
+  },
+  {
+    "revision": "2e095358c35395dc04da9989c5f17f3c",
+    "url": "/website-squad-law-1/static/media/logo-aibox.2e095358.png"
+  },
+  {
+    "revision": "60b408b974fbd9ccf1b322bb5780a890",
+    "url": "/website-squad-law-1/static/media/ness-logo-website.60b408b9.svg"
+  }
+]);
